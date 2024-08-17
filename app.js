bf258a28cd5298cc02f4d56e97ecc891af20edb2
Ravi <ravisankar@campx.in>
2024-08-17 16:37:42 +0530

    backend completed for portfolio

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ import express from 'express';
 import fileUpload from 'express-fileupload';
 import { connectDatabase } from './database/database.js';
 import { errorMiddleware } from './middlewares/error.js';
+
+//routes import 
+
+import messageRoutes from './router/messageRoutes.js';
+import projectRouter from './router/projectRouter.js';
+import skillRouter from './router/skillRouter.js';
+import softwareApplicationRouter from './router/softwareApplicationRouter.js';
+import timelineRouter from './router/timelineRoutes.js';
+import userRoutes from './router/userRouters.js';
 const app = express();
 
 dotenv.config({ path: './config/config.env' })
@@ -21,6 +30,16 @@ app.use(fileUpload({
     useTempFiles: true,
     tempFileDir:'/tmp/'
 }))
+
+
+//Routes
+
+app.use('/api/v1/message', messageRoutes)
+app.use('/api/v1/user', userRoutes)
+app.use("/api/v1/timeline", timelineRouter);
+app.use("/api/v1/softwareapplication", softwareApplicationRouter);
+app.use("/api/v1/skill", skillRouter);
+app.use("/api/v1/project", projectRouter);
 
 
 connectDatabase()
